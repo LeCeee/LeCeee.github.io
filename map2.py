cf63154d57cc940ac7775e9074e20f41bc22ffc1
queenCee <aakarshitatiwari@gmail.com>
2018-09-28 23:01:08 +0530

    cleaned repo

--- map2.py
+++ map2.py
@@ -1,10 +1,26 @@
 import folium
 import pandas
+import phonenumbers
 
-data = pandas.read_csv("predictionSmall.csv")
+data = pandas.read_csv("predictionsWeb.csv")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+city = list(data["city"])
+helpn = list(data["helplineNumber"])
 stat = list(data["UNSAFE"])
+hosp = list(data["hospital"])
+hospc = list(data["hospitalContact"])
+pols = list(data["policeStation"])
+
+# print(type(helpn))
+for idx in range(len(helpn)):
+    helpn[idx] = '011'+ str(helpn[idx])
+    hospc[idx] = '011'+ str(hospc[idx])
+
+
+# print(helpn)
+    # print(n)
+
 # elev = list(data["ELEV"])
 # name = list (data["NAME"])
 def color_prod (st) :
@@ -13,15 +29,12 @@ def color_prod (st) :
     else : return "red"
 
 
-map = folium.Map(location =[28.68, 77.01], zoom_start = 11, tiles = "Mapbox Street", API_key = 'pk.eyJ1IjoiY2VlZSIsImEiOiJjam1sc2lmaWwwYXAxM3FvYWN2dTh3ejNjIn0.efl2Y5sXJ_58TCpNhpSVbg', attr='Mapbox Data Attribution')
+map = folium.Map(location =[28.67, 77.09], zoom_start = 15, tiles = "Mapbox Street", API_key = 'pk.eyJ1IjoiY2VlZSIsImEiOiJjam1sc2lmaWwwYXAxM3FvYWN2dTh3ejNjIn0.efl2Y5sXJ_58TCpNhpSVbg', attr='Mapbox Data Attribution')
 fg = folium.FeatureGroup(name = "My Map2")
-for lt ,ln ,st in zip(lat,lon,stat) :
-    fg.add_child(folium.Marker(location=[lt,ln], popup=folium.Popup(f'status : {st}',parse_html=True), icon=folium.Icon(color=color_prod(st))))
 
-    # if st == 1.0 :
-    #     fg.add_child(folium.Marker(location=[lt,ln], popup=folium.Popup(f'status : {st}',parse_html=True), icon=folium.Icon(color='green')))
-    # else :
-    #     fg.add_child(folium.Marker(location=[lt,ln], popup=folium.Popup(f'status : {st}',parse_html=True), icon=folium.Icon(color='red')))
+for lt ,ln ,st,hn,hos, hosc, pol, ct in zip(lat,lon,stat,helpn,hosp,hospc,pols,city) :
+    fg.add_child(folium.Marker(location=[lt,ln], popup=folium.Popup(f' Area : {ct}, Nearest police station: {pol} Helpline Number :{str(hn)}, Nearest hospital : {str(hos)} Helpline number : {hosc}',parse_html=True), icon=folium.Icon(color=color_prod(st))))
+
 
 map.add_child(fg)
 folium.TileLayer('openstreetmap').add_to(map)
